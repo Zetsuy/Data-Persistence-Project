e2fc30cee05c8ca87abd887a4b3b0fb72ebe9993
Zetsuy <jeremygarciamobile@gmail.com>
2023-04-05 09:45:21 +0200

    Finish Project

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,23 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-   
-    private void Start()
-    {
-        
-    }
+    [SerializeField] Text InputField;
     
     public void StartNew()
     {
         SceneManager.LoadScene(1);
+    }
+    
+    public void SetPlayerName()
+    {
+        PlayerDataHandle.Instance.PlayerName = InputField.text;
+    }
+
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+        #else
+                Application.Quit();
+        #endif
     }
     
     
